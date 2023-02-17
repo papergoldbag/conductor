0841f3846e76e02e70611f281e2960c9cd243f09
papergoldbag <sabarsenrash@gmail.com>
2023-02-18 01:52:52 +0500

    res

--- conductor/api/dependencies.py
+++ conductor/api/dependencies.py
@@ -1,11 +1,14 @@
+from typing import Optional
+
 from fastapi import Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
+from conductor.core.misc import db
 from conductor.db.models import User
 
 
-# def get_current_user(ac: HTTPAuthorizationCredentials = Security(HTTPBearer())) -> User:
-#     user = db.user_by_token(s, token=ac.credentials)
-#     if not user:
-#         raise HTTPException(status.HTTP_403_FORBIDDEN, "Not authenticated")
-#     return
+def get_current_user(ac: HTTPAuthorizationCredentials = Security(HTTPBearer())) -> Optional[User]:
+    user = db.user.pymongo_collection.find_one({'tokens': {"$in": [ac, ]}})
+    if user is None:
+        return None
+    return User.parse_document(user)
