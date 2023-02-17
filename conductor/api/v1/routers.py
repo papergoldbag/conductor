4512831c0d1ba44905927d50a8cef8eea63e1552
papergoldbag <sabarsenrash@gmail.com>
2023-02-18 00:49:27 +0500

    res

--- conductor/api/v1/routers.py
+++ conductor/api/v1/routers.py
@@ -10,11 +10,13 @@ async def create_user(user: CreateUser):
     ...
 
 
+# TODO
 @users_router.get('/', response_model=UsersResponse)
 async def get_user(user_id: int):
     ...
 
 
+# TODO
 @users_router.get('/roadmap/', response_model=RoadmapsResponse)
 async def get_user_roadmap(user_id: int):
     ...
@@ -28,6 +30,7 @@ async def create_roadmap(roadmap: CreateRoadmap):
     ...
 
 
+# TODO
 @roadmaps_router.get('/templates/', response_model=RoadmapsResponse)
 async def get_template_roadmap(template_roadmap_id: int):
     ...
