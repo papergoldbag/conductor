a701485bcd81b80bc482c88a00da433379351a2f
ReRosu <recrea.tor@yandex.ru>
2023-02-17 23:58:42 +0500

    create schemas

--- conductor/api/schemas/common.py
+++ conductor/api/schemas/common.py
@@ -1,4 +1,5 @@
 from conductor.database.models import *
+from typing import Any, Optional
 
 
 class Statuses:
@@ -18,3 +19,39 @@ class UsersResponse(CommonResponse):
 
 class RoadmapsResponse(CommonResponse):
     out: dict[str, Roadmap]
+
+
+class CreateUser(BaseModel):
+    email: str
+    role: str
+    position: str
+    birth_date: date
+    description: str
+    telegram: str
+    whatsapp: str
+    vk: str
+    roadmap_id: Optional[int]
+    division_id: int
+
+
+class CreateRoadmap(BaseModel):
+    title: str
+    tasks: dict[int, Task]
+    created_by_id: str
+
+
+class Test(BaseModel):
+    question: str
+    answer: str
+    correct: str
+
+
+class CreateTasks(BaseModel):
+    type: str
+    title: str
+    text: str
+    attachments: dict[str, str]
+    test: list[Test]
+    coins: int
+
+
