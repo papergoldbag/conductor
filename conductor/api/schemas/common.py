6a2d3ee2d6b17e8292f709424113fe8e39d7df89
ReRosu <recrea.tor@yandex.ru>
2023-02-17 23:14:30 +0500

    add schemas

--- conductor/api/schemas/common.py
+++ conductor/api/schemas/common.py
@@ -1,4 +1,5 @@
 from typing import Any, Optional
+from datetime import date
 
 from pydantic import BaseModel
 
@@ -12,3 +13,49 @@ class CommonResponse(BaseModel):
     status: str = Statuses.OK
     out: dict[str, Any]
     err: Optional[str] = None
+
+class User(BaseModel):
+    id : str
+    email : str
+    tokens : list[str]
+    role : str
+    coins : int
+    position : str
+    birth_date : date
+    description : str
+    telegram : str
+    whatsapp : str
+    vk : str
+    roadmap_id : str
+    division_id : str
+
+
+class UsersResponse(CommonResponse):
+    out : dict[str, User]
+
+
+class Task(BaseModel):
+    id : str
+    type : str
+    title : str
+    text : str
+    attachments : list[str]
+    test : list[dict[str, str]]
+    marked_completed_by_id : str
+    coins : int
+
+
+class TasksResponse(CommonResponse):
+    out : dict[str, Task]
+
+
+class Roadmap(BaseModel):
+    id : str
+    title : str
+    tasks : list[Task]
+    created_by_id : str
+
+
+class RoadmapsResponse(CommonResponse):
+    out : dict[str, Roadmap]
+
