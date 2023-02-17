e0fa5e5da2787a6786624a85c69213ffc246acef
ReRosu <recrea.tor@yandex.ru>
2023-02-17 23:14:24 +0500

    add routes to general v1 route

--- conductor/api/v1/__init__.py
+++ conductor/api/v1/__init__.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
 from conductor.api.v1 import echo
+from conductor.api.v1 import routers
 
 api_v1_router = APIRouter(prefix='/v1')
 api_v1_router.include_router(echo.router, prefix='/echo')
+api_v1_router.include_router(routers.users_router, prefix='/user')
+api_v1_router.include_router(routers.roadmaps_router, prefix='/roadmap')
