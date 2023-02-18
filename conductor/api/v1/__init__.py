7395578db87875bf9d42ac99fd57cd055fed6fbc
papergoldbag <sabarsenrash@gmail.com>
2023-02-19 03:30:30 +0500

    res

--- conductor/api/v1/__init__.py
+++ conductor/api/v1/__init__.py
@@ -12,10 +12,10 @@ from conductor.api.v1.user import user_router
 from conductor.api.v1.role import role_router
 
 api_v1_router = APIRouter(prefix='/v1')
-api_v1_router.include_router(echo_router, prefix='/echo', tags=['echo'])
-api_v1_router.include_router(auth_router, prefix='/auth', tags=['auth'])
-api_v1_router.include_router(me_router, prefix='/me', tags=['me'])
-api_v1_router.include_router(division_router, prefix='/divisions', tags=['divisions'])
+api_v1_router.include_router(echo_router, prefix='/echo', tags=['Echo'])
+api_v1_router.include_router(auth_router, prefix='/auth', tags=['Auth'])
+api_v1_router.include_router(me_router, prefix='/me', tags=['Me'])
+api_v1_router.include_router(division_router, prefix='/divisions', tags=['Divisions'])
 api_v1_router.include_router(user_router, prefix='/user', tags=['Users'])
 api_v1_router.include_router(roadmap_template_router, prefix='/roadmap_template', tags=['Roadmap Templates'])
 api_v1_router.include_router(quizz_router, prefix='/send_answers', tags=['Send Answers'])
