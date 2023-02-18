6ff0d59dec8a70702d6265182ddb80f810bed642
papergoldbag <sabarsenrash@gmail.com>
2023-02-18 05:46:20 +0500

    res

--- conductor/api/v1/me.py
+++ conductor/api/v1/me.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from starlette import status
 
 from conductor.api.dependencies import get_current_user
 from conductor.core.misc import db
@@ -11,6 +12,8 @@ me_router = APIRouter()
 
 @me_router.get('.my_roadmap', response_model=Optional[RoadmapDBM])
 async def my_roadmap(user: UserDBM = Depends(get_current_user)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     if user.roadmap_int_id is None:
         return {}
     roadmap = RoadmapDBM.parse_document(db.roadmap.get_document_by_int_id(user.roadmap_int_id))
