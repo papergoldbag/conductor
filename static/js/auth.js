46890c88cf776a76c6a7cd5b12b263c276011e34
PirateThunder <ilyakhakimov03@gmail.com>
2023-02-18 05:02:33 +0500

    added mnogo chego

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,11 +1,67 @@
-// window.onload = () => {
-//     const form = document.querySelector("form")
+window.onload = () => {
+    const form = document.querySelector("form")
+    const codeSendButton = document.querySelector(".codeSendButton")
 
-//     const validateAuth = (event) => {
-//         event.preventDefault()
-//         let formData = new FormData(form)
-//         // validate formData
-//     }
+    const validateEmail = (email) => {
+        return String(email)
+        .toLowerCase()
+        .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    };
 
-//     form.addEventListener("submit", validateAuth)
-// }
+    async function validateAuth(event) {
+        // return validateEmail(mail)
+        event.preventDefault()
+        console.log("lol")
+        let formData = new FormData(form)
+
+        // validate formData
+        let mail = formData.get("email")
+        let code = formData.get("code")
+        if (validateEmail(mail)) {
+            let user = {
+                mail: mail,
+                code: code
+            }
+            console.log(user)
+            const settings = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+                body: JSON.stringify(user)
+            }
+            fetch("https://divarteam.ru/api/v1/auth", settings)
+            .then(data => {
+                console.log(data)
+            })
+        } else {
+            alert("Некорректный email!")
+        }
+
+        // for (let [k, v] of formData) {
+        //     console.log(k, v)
+        // }
+    }
+
+    async function codeSendButtonHandler() {
+        let mail = document.querySelector(".email").value
+        if (validateEmail(mail)) {
+            const settings = {
+                // headers: {
+                //     "token": 'uaqFXhzjxuiVNfyTmSGeVfjukPfSDihfTifTShMtaMOaQRpFAl'
+                // }
+            }
+            // проверка мыла
+            let url = "https://divarteam.ru/api/v1/auth.send_mail_code?mail=" + mail
+            console.log(url)
+            await fetch(url, settings)
+        } else {
+            alert("Некорректный email!")
+        }
+    }
+
+    form.addEventListener("submit", validateAuth)
+    codeSendButton.addEventListener("click", codeSendButtonHandler)
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,37 @@
 window.onload = () => {
-    const headerMenu = document.querySelector(".menu-button")
-    let isHeaderMenuOpen = false
+    // const headerMenu = document.querySelector(".menu-button")
+    // let isHeaderMenuOpen = false
 
-    const headerMenuLogic = () => {
-        if (!isHeaderMenuOpen) {
-            isHeaderMenuOpen = true
-        } else {
-            isHeaderMenuOpen = false
-        }
-        console.log(isHeaderMenuOpen)
+    // const headerMenuLogic = () => {
+    //     if (!isHeaderMenuOpen) {
+    //         isHeaderMenuOpen = true
+    //     } else {
+    //         isHeaderMenuOpen = false
+    //     }
+    //     console.log(isHeaderMenuOpen)
+    // }
+
+    // headerMenu.addEventListener("click", headerMenuLogic)
+
+    const dropdownTriggers = document.querySelectorAll("nav .dropdown-trigger")
+    console.log(dropdownTriggers)
+
+    for (let i = 0; i < dropdownTriggers.length; i++) {
+        dropdownTriggers[i].addEventListener("click", function() {
+            let flag = (this.getAttribute("data-isopen") === 'true')
+            console.log(flag)
+            console.log(this.nextElementSibling)
+            if (flag) {
+                this.nextElementSibling.style.display = "none"
+            } else {
+                this.nextElementSibling.style.display = "block"
+            }
+            this.setAttribute("data-isopen", (!flag).toString())
+            console.log(this)
+        })
     }
 
-    headerMenu.addEventListener("click", headerMenuLogic)
+    function foo(el) {
+        console.log(el)
+    }
 }
