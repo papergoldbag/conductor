407ddb3c5dc5e968be12eceb9c8d07bc22786bd7
papergoldbag <sabarsenrash@gmail.com>
2023-02-18 16:51:42 +0500

    res

--- conductor/site/pages.py
+++ conductor/site/pages.py
@@ -31,6 +31,13 @@ async def roadmap(r: Request, user=Depends(get_current_user)):
     return templates.TemplateResponse("roadmap.html", {'request': r})
 
 
+@pages_router.get('/adduser')
+async def adduser(r: Request, user=Depends(get_current_user)):
+    if not user:
+        return RedirectResponse('/adduser', status_code=status.HTTP_302_FOUND)
+    return templates.TemplateResponse("adduser.html", {'request': r})
+
+
 @pages_router.get('/sign_out')
 async def roadmap(
         r: Request,
