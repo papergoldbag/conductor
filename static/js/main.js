221062a543d0503fbb572ae77295a9300c865edc
PirateThunder <ilyakhakimov03@gmail.com>
2023-02-18 06:44:02 +0500

    f

--- static/js/main.js
+++ static/js/main.js
@@ -12,18 +12,20 @@ window.onload = () => {
     // }
 
     // headerMenu.addEventListener("click", headerMenuLogic)
-    let roadmap = {
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        7: [],
-        8: [],
-        9: [],
-        10: []
-    }
+    let raw_roadmap = {}
+    let roadmap = []
+    // let roadmap = {
+    //     1: [],
+    //     2: [],
+    //     3: [],
+    //     4: [],
+    //     5: [],
+    //     6: [],
+    //     7: [],
+    //     8: [],
+    //     9: [],
+    //     10: []
+    // }
 
     const dropdownTriggers = document.querySelectorAll("nav .dropdown-trigger")
     //console.log(dropdownTriggers)
@@ -54,12 +56,25 @@ window.onload = () => {
         fetch("/api/v1/me.my_roadmap", settings)
         .then(response => response.json())
         .then(data => {
-            // console.log(data.tasks)
-            for (let task of data.tasks) {
-                roadmap[task.week_num].push(task)
+            console.log(data)
+            // for (let task of data.tasks) {
+            //     roadmap[task.week_num].push(task)
+            // }
+            for (let week of data.weeks) {
+                roadmap[week] = []
+            }
+            for (let [week, arrDay] of data.week_to_days) {
+                console.log(week, arrDay)
+                // for (let day of arrDay) {
+                //     let o = {day: []}
+                //     roadmap[week].push( o )
+                // }
             }
         })
-        console.log(roadmap)
+        console.log(roadmap) 
+        // for (let arr of roadmap) {
+        //     arr.sort((a, b) => a.day_num)
+        // }
     }
 
     loadRoadmap()
