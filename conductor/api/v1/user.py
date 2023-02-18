22765dbefbaf48105254b15b334cab4a19b4f173
ReRosu <recrea.tor@yandex.ru>
2023-02-18 07:15:47 +0500

    create user route

--- conductor/api/v1/user.py
+++ conductor/api/v1/user.py
@@ -2,10 +2,16 @@ from fastapi import APIRouter, Depends
 
 from conductor.api.dependencies import make_strict_depends_on_roles
 from conductor.db.models import UserDBM, Roles
+from conductor.api.schemas.user import CreateUser
+from conductor.core.misc import db
 
 user_router = APIRouter()
 
 
 @user_router.post(".create")
-def create_user(user: UserDBM = Depends(make_strict_depends_on_roles(roles=[Roles.hr, Roles.supervisor]))):
-    return {}
+def create_user(user: UserDBM = Depends(make_strict_depends_on_roles(roles=[Roles.hr, Roles.supervisor])), user_to_create: CreateUser = None):
+    user = UserDBM.parse_document(db.user.insert_document(user_to_create.dict()))
+    return user
+
+
+
