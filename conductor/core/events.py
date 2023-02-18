9332fc9fcb298bb15c9271a4c828aed504640b08
papergoldbag <sabarsenrash@gmail.com>
2023-02-18 10:40:47 +0500

    res

--- conductor/core/events.py
+++ conductor/core/events.py
@@ -1,10 +1,14 @@
 import logging
 
+from conductor.core.misc import settings
+from conductor.testdata.insert_test_data import insert_test_data
+
 log = logging.getLogger(__name__)
 
 
 async def on_startup():
-    ...
+    if settings.prod_mode:
+        insert_test_data()
 
 
 async def on_shutdown():
