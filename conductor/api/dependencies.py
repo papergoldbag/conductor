d4a3bd2998a66eabfdeb63c4dc5c62d19ded6ef5
papergoldbag <sabarsenrash@gmail.com>
2023-02-18 05:01:06 +0500

    res

--- conductor/api/dependencies.py
+++ conductor/api/dependencies.py
@@ -1,15 +1,25 @@
 from typing import Optional
 
-from fastapi import Security, Depends, HTTPException
+from fastapi import Security, Depends, HTTPException, Header
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from starlette import status
+from starlette.requests import Request
 
 from conductor.core.misc import db
 from conductor.db.models import UserDBM
 
 
-def get_current_user(ac: HTTPAuthorizationCredentials = Security(HTTPBearer())) -> UserDBM:
-    user = db.user.pymongo_collection.find_one({'tokens': {"$in": [ac.credentials]}})
+def get_current_user(*, token: str = Header(None), req: Request) -> UserDBM:
+    token_cookie: str = req.cookies.get('token')
+    if not token and not token_cookie:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid auth'
+        )
+
+    token = token if token else token_cookie
+
+    user = db.user.pymongo_collection.find_one({'tokens': {"$in": [token]}})
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='bad token')
     return UserDBM.parse_document(user)
---
+++ conductor/site/pages.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+from starlette import status
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+
+from conductor.api.dependencies import get_current_user
+from conductor.core.misc import templates
+
+pages_router = APIRouter(tags=['pages'])
+
+
+@pages_router.get('/auth')
+async def auth(r: Request, user=Depends(get_current_user)):
+    if user:
+        return RedirectResponse('/roadmap', status_code=status.HTTP_302_FOUND)
+    return templates.TemplateResponse("auth.html", {'request': r})
+
+
+@pages_router.get('/roadmap')
+async def roadmap(r: Request, user=Depends(get_current_user)):
+    if not user:
+        return RedirectResponse('/auth', status_code=status.HTTP_302_FOUND)
+    return templates.TemplateResponse("roadmap.html", {'request': r})
