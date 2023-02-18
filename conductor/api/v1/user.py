966118f32a27ae6be9d7f84eb2feac6558286d48
ReRosu <recrea.tor@yandex.ru>
2023-02-18 08:11:14 +0500

    fix check

--- conductor/api/v1/user.py
+++ conductor/api/v1/user.py
@@ -12,7 +12,7 @@ user_router = APIRouter()
 
 @user_router.post(".create")
 def create_user(user: UserDBM = Depends(make_strict_depends_on_roles(roles=[Roles.hr, Roles.supervisor])), user_to_create: CreateUser = None):
-    if user_to_create.role == 'supervisor' and user.role == 'hr':
+    if user_to_create.role in ('supervisor', 'hr') and user.role == 'hr':
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='hr cant create supervisor')
     user_ = UserDBM(tokens=[], telegram='', whatsapp='', vk='', coins=0, **user_to_create.dict())
     inserted = UserDBM.parse_document(db.user.insert_document(user_.document()))
