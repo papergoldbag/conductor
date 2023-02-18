64b98eeecd0c737c770c0a97dba93401210f2ea8
PirateThunder <ilyakhakimov03@gmail.com>
2023-02-18 18:45:00 +0500

    role

---
+++ static/js/profile.js
@@ -0,0 +1,15 @@
+window.onload = () => {
+    async function checkRole() {
+        console.log('test')
+        fetch("/api/v1/me.my_profile")
+        .then(data => data.json())
+        .then(data => {
+            let role = data.role
+            if (role == 'hr') {
+                document.querySelector('.header-links').innerHTML += `<a href="/adduser"><button class="secondary">Управление</button></a>`
+            }
+        })
+    }
+
+    checkRole()
+}
