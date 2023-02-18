04d61602240a6ef47eda578255fe8e292deac1d0
ReRosu <recrea.tor@yandex.ru>
2023-02-18 08:08:38 +0500

    fix create user route

--- conductor/api/v1/user.py
+++ conductor/api/v1/user.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, Depends
+from fastapi import HTTPException, Body
+from starlette import status
 
 from conductor.api.dependencies import make_strict_depends_on_roles
 from conductor.db.models import UserDBM, Roles
@@ -10,8 +12,10 @@ user_router = APIRouter()
 
 @user_router.post(".create")
 def create_user(user: UserDBM = Depends(make_strict_depends_on_roles(roles=[Roles.hr, Roles.supervisor])), user_to_create: CreateUser = None):
-    user = UserDBM(tokens=[], telegram='', whatsapp='', vk='', coins=0, **user_to_create.dict())
-    inserted = UserDBM.parse_document(db.user.insert_document(user.document()))
+    if user_to_create.role == 'supervisor' and user.role == 'hr':
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='hr cant create supervisor')
+    user_ = UserDBM(tokens=[], telegram='', whatsapp='', vk='', coins=0, **user_to_create.dict())
+    inserted = UserDBM.parse_document(db.user.insert_document(user_.document()))
     return inserted
 
 
