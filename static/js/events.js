cb502de20917cd2c564e2d29b2eddc4275aa65db
PirateThunder <ilyakhakimov03@gmail.com>
2023-02-19 05:19:23 +0500

    m

--- static/js/events.js
+++ static/js/events.js
@@ -1,6 +1,27 @@
 window.onload = () => {
+    const mainHTML = document.querySelector('main')
+
+    async function loadEvents() {
+        fetch('/api/v1/me.get_my_events')
+        .then(data => data.json())
+        .then(data => {
+            console.log(data)
+            let htm = ''
+            for (let event_ of data) {
+                htm += `
+                <div class="card grey" style="width: 98%;">
+                    <h2>${event_.title}</h2>
+                    <p>${event_.desc}</p>
+                    <b>${event_.dt.substring(0, 10)}</b>
+                </div>
+                `
+            }
+            //console.log(mainHTML)
+            mainHTML.innerHTML = htm
+        })
+    }
+
     async function checkRole() {
-        console.log('test')
         fetch("/api/v1/me.my_profile")
         .then(data => data.json())
         .then(data => {
@@ -12,4 +33,5 @@ window.onload = () => {
     }
 
     checkRole()
+    loadEvents()
 }
